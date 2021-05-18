94bf7552bb3afa6f5aaec689166d2306d3e2d2e8
Roman <roma.aikevich@mail.ru>
2021-05-18 12:11:25 +0300

    Initial Commit

---
+++ vpn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vpn
+//
+//  Created by Roman Alikevich on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
